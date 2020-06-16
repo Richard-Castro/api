94cc2d31d997cca8b84ae99f2078ec3e1685b8e9
Maicon Moreno <maiconmoreno@hotmail.com>
2020-06-16 17:32:22 -0300

    Inclusão da lib cors

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 // [START app]
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello from App Engine - Cabana!");
